fb811fb677edeac6a4728235e0aa18d507bc1df7
chris-rass <87675538+chris-rass@users.noreply.github.com>
2021-07-28 20:36:51 -0400

    update rest api

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbwzppQKNmNOcO7l3r96BY_KCg6yqOntokPTwLRI/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwmUO4Gq7uOiPVF-K0EzihroW6OaXeyc3hKIa1RFVctg-tmIPI/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
